3a4f84fede658af8491d527ebb17def2c9e331cd
Nexiva Tech <nexivatech@gmail.com>
2025-09-04 13:25:51 +0500

    ....

--- index.js
+++ index.js
@@ -346,10 +346,10 @@ app.use((error, req, res, next) => {
   });
 });
 
-app.listen(PORT, () => {
-  console.log(`🚀 Server is running on http://localhost:${PORT}`);
-  console.log(`📧 Email will be sent to: nexivatech@gmail.com`);
-  console.log(`📁 File uploads directory: ${uploadsDir}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`🚀 Server is running on http://localhost:${PORT}`);
+//   console.log(`📧 Email will be sent to: nexivatech@gmail.com`);
+//   console.log(`📁 File uploads directory: ${uploadsDir}`);
+// });
 
 module.exports = app;
